6097c13495af67bb95395ed59a335574c22cf445
Baastheglass <122256730+Baastheglass@users.noreply.github.com>
2025-06-11 18:00:54 +0500

    adding basic scraping architecture and bs4

--- back_end/scrape.py
+++ back_end/scrape.py
@@ -1,8 +1,15 @@
 import requests
 from googlesearch import search
+from bs4 import BeautifulSoup
 
 if __name__ == "__main__":
+    shopping_sites = open("shopping_sites.txt", "r").read().splitlines()
+    print(shopping_sites)
     query = input("Enter query: ")
-    search_results = search(query, num_results=100)
+    search_results = search(query, num_results=10)
     for result in search_results:
-        print(result)
+        for site in shopping_sites:
+            if site in result:
+                response = requests.get(result)
+                soup = BeautifulSoup(response.text, 'html.parser')
+                print(result)
