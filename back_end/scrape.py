ac7faf78ca0748fe0b2802b018821754a51d2148
Baastheglass <122256730+Baastheglass@users.noreply.github.com>
2025-06-12 15:51:12 +0500

    Added basic infrastructure for habitt

--- back_end/scrape.py
+++ back_end/scrape.py
@@ -4,12 +4,31 @@ from bs4 import BeautifulSoup
 
 if __name__ == "__main__":
     shopping_sites = open("shopping_sites.txt", "r").read().splitlines()
-    print(shopping_sites)
     query = input("Enter query: ")
     search_results = search(query, num_results=10)
     for result in search_results:
+        print(result)
         for site in shopping_sites:
             if site in result:
+                print("If entered")
                 response = requests.get(result)
                 soup = BeautifulSoup(response.text, 'html.parser')
-                print(result)
+                if site == "habitt":
+                    base_habbit_link = "https://habitt.com"
+                    prices = soup.find_all("span", class_="price-item price-item--regular")
+                    image_links = []
+                    card_links = soup.find_all("a", class_ = "card-link")
+                    for link in card_links:
+                        response = requests.get(base_habbit_link + link.get("href"))
+                        soup = BeautifulSoup(response.text, 'html.parser')
+                        possible_image_links = soup.find_all("img")
+                        possible_image_links = list(possible_image_links)
+                        try:
+                            for link in possible_image_links:
+                                if "product-featured-image" in link.get("id"):
+                                    image_links.append(link.get("src"))
+                            print(image_links)
+                            break  
+                        except Exception as e:
+                            print(e)
+                #print(soup)
