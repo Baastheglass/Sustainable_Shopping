088e6b7922a2be02ddd94dadfe8fffa3dda29811
Baastheglass <lightningblazer123@gmail.com>
2025-06-17 23:30:43 +0500

    basic api setup and code refactoring

--- back_end/scrape.py
+++ back_end/scrape.py
@@ -45,6 +45,30 @@ def daraz(linky):
                 print("Price found: ", span.get_attribute("innerText")) #accurately getting product price
         print("Iter done")
 
+def habitt(linky):
+    response = requests.get(linky)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    base_habbit_link = "https://habitt.com"
+    prices = soup.find_all("span", class_="price-item price-item--regular")
+    image_links = []
+    card_links = soup.find_all("a", class_ = "card-link")
+    for link in card_links:
+        response = requests.get(base_habbit_link + link.get("href"))
+        soup = BeautifulSoup(response.text, 'html.parser')
+        possible_image_links = soup.find_all("img")
+        possible_image_links = list(possible_image_links)
+        try:
+            for link in possible_image_links:
+                #print(link.get("id"))
+                if(link.get("id") != None):
+                    if "product-featured-image" in link.get("id"):
+                        image_link = link.get("src")
+                        image_link = str(image_link)[2:]
+                        image_links.append(image_link) 
+        except Exception as e:
+            print(e)
+    print(image_links)
+
 if __name__ == "__main__":
     shopping_sites = open("./shopping_sites.txt", "r").read().splitlines()
     query = input("Enter query: ")
@@ -73,30 +97,9 @@ if __name__ == "__main__":
         for site in shopping_sites:
             if site in result:
                 #print("If entered")
-                response = requests.get(result)
-                soup = BeautifulSoup(response.text, 'html.parser')
+                
                 if site == "habitt":
-                    pass
-                    # base_habbit_link = "https://habitt.com"
-                    # prices = soup.find_all("span", class_="price-item price-item--regular")
-                    # image_links = []
-                    # card_links = soup.find_all("a", class_ = "card-link")
-                    # for link in card_links:
-                    #     response = requests.get(base_habbit_link + link.get("href"))
-                    #     soup = BeautifulSoup(response.text, 'html.parser')
-                    #     possible_image_links = soup.find_all("img")
-                    #     possible_image_links = list(possible_image_links)
-                    #     try:
-                    #         for link in possible_image_links:
-                    #             #print(link.get("id"))
-                    #             if(link.get("id") != None):
-                    #                 if "product-featured-image" in link.get("id"):
-                    #                     image_link = link.get("src")
-                    #                     image_link = str(image_link)[2:]
-                    #                     image_links.append(image_link) 
-                    #     except Exception as e:
-                    #         print(e)
-                    # print(image_links)
+                    habitt(result)
                 elif site == "daraz":
                     try:
                         daraz(result)    
